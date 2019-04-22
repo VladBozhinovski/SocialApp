6a0ea0e6321ac840630ebde4a5b8c24e7fa4758e
vladimir.bozhinovski <vladimir.bozhinovski@musala.com>
2019-04-22 14:09:04 +0200

    Authorization service added (login and register)

--- SocialApp.API/Data/DataContext.cs
+++ SocialApp.API/Data/DataContext.cs
@@ -1,5 +1,6 @@
 using DatingApp.API.models;
 using Microsoft.EntityFrameworkCore;
+using SocialApp.API.models;
 
 namespace DatingApp.API.Data
 {
@@ -11,5 +12,7 @@ namespace DatingApp.API.Data
         }
 
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
+        
     }
 }
